9a75905a4134d67dbbffd890eb22af542402b4e0
tahreen zeba <tahreenzeba1@gmail.com>
2020-02-13 02:29:05 -0500

    Fix a typo mistake

--- models/Post.js
+++ models/Post.js
@@ -1,52 +1,56 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
+// Create Schema
 const PostSchema = new Schema({
-    users: {
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
         type: Schema.Types.ObjectId,
-        ref:'users'
-    },
-    text:{
-        type:String,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
         required: true
-    },
-    name:{
-        type:String
-    },
-    avatar: {
+      },
+      name: {
         type: String
-    },
-    likes:[
-        {
-            user:{
-                type: Schema.Types.ObjectId,
-                ref:'users'
-            }
-        }
-    ],
-    comments: [
-        {
-            user:{
-                type: Schema.Types.ObjectId,
-                ref:'users'
-            },
-            text:{
-                type: String
-            },
-            avatar:{
-                type: String
-            },
-            date:{
-                type: Date,
-                default: Date.now
-            }
-        }
-    ],
-    date: {
+      },
+      avatar: {
+        type: String
+      },
+      date: {
         type: Date,
         default: Date.now
+      }
     }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = PostSchema = mongoose.model('post', PostSchema);
+module.exports = Post = mongoose.model('post', PostSchema);
