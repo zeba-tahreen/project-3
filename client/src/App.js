c7949f15744d495382b8bea0c8928ed93be4ccba
tahreen zeba <tahreenzeba1@gmail.com>
2020-02-14 07:23:28 -0500

    Add new components for private and dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,16 @@ import Landing from './components/layouts/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layouts/Alert';
-import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setAuthToken';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+
 
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
 
 import './App.css';
 
@@ -34,6 +38,7 @@ const App = () => {
         <Switch>
           <Route exact path ='/register' component={Register} />
           <Route exact path ='/login' component={Login} />
+          <PrivateRoute exact path ='/dashboard' component={Dashboard} />
   
         </Switch>
       </section>
