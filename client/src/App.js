d77464e98ecbb49d815bb224a3f822828be49939
tahreen zeba <tahreenzeba1@gmail.com>
2020-02-14 04:00:08 -0500

    Build front end with react

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layouts/Navbar';
+import Landing from './components/layouts/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Alert from './components/layouts/Alert';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+import './App.css';
+
+if(localStorage.token){
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+      <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Alert />
+        <Switch>
+          <Route exact path ='/register' component={Register} />
+          <Route exact path ='/login' component={Login} />
+  
+        </Switch>
+      </section>
+      <Landing />
+    </Fragment>
+    </Router>
+    </Provider>
+  )
+  
+}
+
+
+export default App;
