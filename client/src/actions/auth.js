bd662bc920d8a497d663364ebc0071e90f9b198e
tahreen zeba <tahreenzeba1@gmail.com>
2020-02-18 01:57:42 -0500

    add redirect after login to view profile

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,6 +7,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  CLEAR_PROFILE,
   LOGOUT
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
@@ -100,5 +101,7 @@ export const login = ({ email, password }) => async dispatch => {
   // LOGOUT / clear profile
 
   export const logout = () => dispatch => {
+    dispatch({ type: CLEAR_PROFILE});
     dispatch({ type: LOGOUT});
+
   }
--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -4,34 +4,38 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
 
-const Navbar = ({ auth:{ isAuthenticated, loading}, logout }) => {
-  
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
-        <li>
-          <Link onClick={logout} to='#!'>
-            <i className="fas fa-sign-out-alt"></i>{' '}
-            <span className="hide-sm">Logout</span>
-          </Link>
-        </li>
-      </ul>
-
+      <li>
+        <Link to='/dashboard'>
+          <i className='fas fa-user'></i>
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
+      </li>
+      <li>
+        <Link onClick={logout} to='#!'>
+          <i className='fas fa-sign-out-alt'></i>{' '}
+          <span className='hide-sm'>Logout</span>
+        </Link>
+      </li>
+    </ul>
   );
 
   const guestLinks = (
-<ul>
-        <li>
-          <Link to='#!'>Developers</Link>
-        </li>
-        <li>
-          <Link to='/register'>Register</Link>
-        </li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-      </ul>
-  )
-  
+    <ul>
+      <li>
+        <Link to='#!'>Developers</Link>
+      </li>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+    </ul>
+  );
+
   return (
     <nav className='navbar bg-dark'>
       <h1>
@@ -39,9 +43,9 @@ const Navbar = ({ auth:{ isAuthenticated, loading}, logout }) => {
           <i className='fas fa-code'></i> DevConnector
         </Link>
       </h1>
-      { !loading && (<Fragment>
-        {isAuthenticated ? authLinks : guestLinks }
-      </Fragment>  )}
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
     </nav>
   );
 };
@@ -49,10 +53,10 @@ const Navbar = ({ auth:{ isAuthenticated, loading}, logout }) => {
 Navbar.propTypes = {
   logout: PropTypes.func.isRequired,
   auth: PropTypes.func.isRequired
-}
+};
 
 const mapStateToProps = state => ({
   auth: state.auth
-})
+});
 
-export default connect(mapStateToProps, {logout}) (Navbar);
+export default connect(mapStateToProps, { logout })(Navbar);
