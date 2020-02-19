f81f4b9afa1a980f33699806d8b32a7c951cb7c4
tahreen zeba <tahreenzeba1@gmail.com>
2020-02-19 03:58:03 -0500

    import dashboardActions in dashboard component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layouts/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
+import DashboardActions from './DashboardActions';
 import { Link } from 'react-router-dom';
 
 const Dashboard = ({
@@ -22,7 +23,9 @@ const Dashboard = ({
         <i className='fas fa-user'></i> Welcome {user && user.name}
       </p>
       { profile !== null ? 
-      <Fragment>Has </Fragment> 
+      <Fragment>
+        <DashboardActions />
+      </Fragment> 
       : 
       <Fragment>
           <p>You have not yet setup a profile, please add some info</p>
