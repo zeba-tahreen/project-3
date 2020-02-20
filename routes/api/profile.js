fb8c47c8d126dc7138eb4a80a449d02ba17dd6d7
tahreen zeba <tahreenzeba1@gmail.com>
2020-02-20 07:48:04 -0500

    typo error fixed

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -339,10 +339,9 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 router.get('/github/:username', (req, res) => {
   try {
     const options = {
-      uri:`https://api.github.com/users/${req.params.username}/repos?per_page=5&
-      sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+      uri:`https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
       method: 'GET',
-      headers: { 'user-agent': 'node.js' }
+      headers: { 'user_agent': 'node.js' }
     };
 
     request(options, (error, response, body) => {
