26ab623108d284d4907eb1050dd2a30064176221
tahreen zeba <tahreenzeba1@gmail.com>
2020-02-21 07:55:17 -0500

    changes to link with postjs

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -10,21 +10,23 @@ const PostItem = ({
   removeLike,
   deletePost,
   auth,
-  post: { _id, text, name, avatar, user, likes, comments, date }
+  post: { _id, text, name, avatar, user, likes, comments, date },
+  showActions
 }) => (
   <div className='post bg-white p-1 my-1'>
     <div>
-      <a href='profile.html'>
+      <Link to={`/profile/${user}`}>
         <img className='round-img' src={avatar} alt='' />
         <h4>{name}</h4>
-      </a>
+      </Link>
     </div>
     <div>
       <p className='my-1'>{text}</p>
       <p className='post-date'>
         Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
       </p>
-      <button
+      {showActions && <Fragment>
+        <button
         onClick={e => addLike(_id)}
         type='button'
         className='btn btn-light'
@@ -39,7 +41,7 @@ const PostItem = ({
       >
         <i className='fas fa-thumbs-down'></i>{' '}
       </button>
-      <Link to={`/post/${_id}`} className='btn btn-primary'>
+      <Link to={`/posts/${_id}`} className='btn btn-primary'>
         Discussion{' '}
         {comments.length > 0 && (
           <span className='comment-count'>{comments.length}</span>
@@ -54,9 +56,15 @@ const PostItem = ({
           <i className='fas fa-times'></i>
         </button>
       )}
+        
+      </Fragment>}
     </div>
   </div>
 );
+
+PostItem.defaultProps = {
+  showActions: true
+}
 
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { getPosts } from '../../actions/post';
 import Spinner from '../layouts/Spinner';
 import PostItem from './PostItem';
+import PostForm from './PostForm';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
@@ -16,7 +17,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
       <i className='fas fa-user'></i> Welcome to the Coummunity
       </p>
     
-          {/*PostForm */}
+          <PostForm />
           <div className='posts'>
               {posts.map(post =>(
                   <PostItem key={post._id} post={post} />
