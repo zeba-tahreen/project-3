204adaa76c686d10365e665046ac88a1c24f5aaf
tahreen zeba <tahreenzeba1@gmail.com>
2020-02-27 08:41:01 -0500

    Typo mistake

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -159,7 +159,7 @@ export const addComment = (postId, formData) => async dispatch => {
 // Delete Comment
 export const deleteComment = (postId, commentId ) => async dispatch => {
   try {
-    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+    const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
 
     dispatch({
       type: REMOVE_COMMENT,
