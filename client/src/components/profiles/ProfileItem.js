a082b30cf918203b9747d99f496ecbbb9a1ad9f9
tahreen zeba <tahreenzeba1@gmail.com>
2020-02-28 05:09:09 -0500

    fix the Id error

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 const ProfileItem = ({
   profile: {
-    user: { _id, name, avatar },
+    user: { id, name, avatar },
     status,
     company,
     location,
@@ -22,7 +22,7 @@ const ProfileItem = ({
       {company && <span> at {company}</span>}
     </p>
     <p className='my-1'>{location && <span>{location}</span>}</p>
-    <Link to={`/profile/${_id}`} className='btn btn-primary'>
+    <Link to={`/profile/${id}`} className='btn btn-primary'>
       View Profile
     </Link>
   </div>
